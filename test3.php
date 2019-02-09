b5266c665d361e50a3b6471e4565d18161cabaef
Suriyavardhan <44000218+vardhan12@users.noreply.github.com>
2019-02-08 22:39:12 -0800

    Add files via upload

---
+++ test3.php
@@ -0,0 +1,15 @@
+<?php
+$x=true;
+$y="hello world";
+echo $x;
+var_dump($x);
+echo "<br>";
+$x=5985.10;
+echo $x;
+echo "<br>";
+var_dump($x);
+$x=false;
+if($x){
+	echo "<br>";
+echo "all is well";}
+?>
